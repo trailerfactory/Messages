b8701c9e7bbf843819e422eca57a20e122048174
trailerfactory <89105913+trailerfactory@users.noreply.github.com>
2023-09-03 19:36:34 +0700

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var myAudio = document.getElementById("myAudio");
+         var myH1 = document.getElementById("myH1");
+         var myP1 = document.getElementById("myP1");
+         var playButton = document.getElementById("playButton");
+         var playing = false;
+
+         function playAudio() {
+            myAudio.play();
+            playing = true;
+            playButton.innerHTML = "Hi there!";
+            playButton.classList.add("active");
+            myH1.style.opacity = "1";
+            myP1.style.opacity = "1";
+         }
+
+         myH1.style.opacity = "0";
+         myP1.style.opacity = "0";
